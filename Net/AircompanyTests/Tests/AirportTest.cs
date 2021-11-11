cf4797354d119dad34ee14a38f11f18ab3fb7f96
Eugeniya Kasperovich <kasperovich.official@gmail.com>
2021-11-12 00:43:13 +0300

    test refactoring

--- Net/AircompanyTests/Tests/AirportTest.cs
+++ Net/AircompanyTests/Tests/AirportTest.cs
@@ -11,31 +11,32 @@ namespace AircompanyTests.Tests
     public class AirportTest
     {
         private List<Plane> _planes = new List<Plane>(){
-           new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
-           new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
-           new PassengerPlane("Boeing-747", 980, 16100, 70500, 242),
-           new PassengerPlane("Airbus A320", 930, 11800, 65500, 188),
-           new PassengerPlane("Airbus A330", 990, 14800, 80500, 222),
-           new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
-           new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
-           new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
-           new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.Bomber),
-           new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.Bomber),
-           new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.Bomber),
-           new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.Fighter),
-           new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.Fighter),
-           new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)
+           new PassengerPlane("P", 0, 0, 2, 999),
+           new PassengerPlane("P", 0, 0, 0, 0),
+           new PassengerPlane("P", 0, 0, 1, 0),
+           new MilitaryPlane("Mi", 0, 0, 3, MilitaryType.Bomber),
+           new MilitaryPlane("Mi", 0, 0, 4, MilitaryType.Fighter),
+           new MilitaryPlane("Tr", 0, 0, 5, MilitaryType.Transport)
         };
 
-        private PassengerPlane planeWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
+        private List<Plane> _planesSortedByMaxLoadCapacity = new List<Plane>(){
+           new PassengerPlane("P", 0, 0, 0, 0),
+           new PassengerPlane("P", 0, 0, 1, 100),
+           new PassengerPlane("P", 0, 0, 2, 0),
+           new MilitaryPlane("Mi", 0, 0, 3, MilitaryType.Bomber),
+           new MilitaryPlane("Mi", 0, 0, 4, MilitaryType.Fighter),
+           new MilitaryPlane("Tr", 0, 0, 5, MilitaryType.Transport)
+        };
 
-        private List<MilitaryPlane> transportMilitaryPlanes = new List<MilitaryPlane> { new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport) };
+        private List<MilitaryPlane> _transportMilitaryPlanes = new List<MilitaryPlane> { new MilitaryPlane("Tr", 0, 0, 5, MilitaryType.Transport) };
+
+        private PassengerPlane _passengerPlaneWithMaxPassengerCapacity = new PassengerPlane("P", 0, 0, 2, 999);
 
         [Test]
         public void Add_MultiplePlanes_ReturnsTransportMilitaryPlanes()
         {
             var airport = new Airport(_planes);
-            Assert.AreEqual(airport.GetTransportMilitaryPlanes(), transportMilitaryPlanes);
+            Assert.AreEqual(airport.GetTransportMilitaryPlanes(), _transportMilitaryPlanes);
         }
 
         [Test]
@@ -43,7 +44,7 @@ namespace AircompanyTests.Tests
         {
             var airport = new Airport(_planes);
             var expectedPassengerPlaneWithMaxCapacity = airport.GetPassengerPlaneWithMaxCapacity();
-            Assert.AreEqual(expectedPassengerPlaneWithMaxCapacity, planeWithMaxPassengerCapacity);
+            Assert.AreEqual(expectedPassengerPlaneWithMaxCapacity, _passengerPlaneWithMaxPassengerCapacity);
         }
 
         [Test]
@@ -51,7 +52,7 @@ namespace AircompanyTests.Tests
         {
             var airport = new Airport(_planes);
             airport.SortByMaxLoadCapacity();
-            CollectionAssert.IsOrdered(airport.Planes);
+            Assert.AreEqual(airport.Planes, _planesSortedByMaxLoadCapacity);
         }
     }
 }
